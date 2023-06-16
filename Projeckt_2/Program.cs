b69891c42fa3d1c1e9ec2a35b5f8c0c811b36527
Grandhald <kel_tua@mail.ru>
2023-06-16 20:03:07 +0300

    weekDay

---
+++ Projeckt_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string inputed = Console.ReadLine();
+int numberDay = Convert.ToInt32(inputed);
+if ( 0<numberDay&numberDay<8)  {
+    string [] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+    Console.WriteLine(week[numberDay-1]);
+}
+else {
+    Console.WriteLine("Вывели не верные данные");
+}
