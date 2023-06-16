a0236e64e995b5535bc59a98f2e83d360cd17097
Grandhald <kel_tua@mail.ru>
2023-06-16 20:22:41 +0300

    Print from minus number to number

---
+++ Project_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+string inputed = Console.ReadLine();
+int number = Convert.ToInt32(inputed);
+int index = -number;
+while (index!=number+1)
+{
+    Console.Write($"{index} ");
+    index++;
+}
