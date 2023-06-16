c6972cc7b42a6a575143d047e885dd82651e42f5
Grandhald <kel_tua@mail.ru>
2023-06-16 19:35:05 +0300

    square number

---
+++ Proejeckt_0/Program.cs
@@ -0,0 +1,6 @@
+﻿string inputedString = Console.ReadLine(); //Ввод данных пользователем
+int number = Convert.ToInt32(inputedString); //перевеод введенных данных в цело-числовый тип данных
+Console.WriteLine("Вы ввели число " + number); //Вывод того, что ввел пользователь
+int square = number * number; // присвоение перменой square значения квадарата числа number
+Console.WriteLine($"Квадарт числа {number} равен {square}"); // Вывод квадрата числа
+
