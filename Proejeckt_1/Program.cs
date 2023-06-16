f47135cd21f6a4d5113dae2abed6e0ac374a4421
Grandhald <kel_tua@mail.ru>
2023-06-16 19:36:51 +0300

    numerSeond equals square numberFirst

---
+++ Proejeckt_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+string inputFirst = Console.ReadLine();
+int numberFirst = Convert.ToInt32(inputFirst);
+Console.WriteLine("Введите второе число: ");
+string inputSecond = Console.ReadLine();
+int numberSecond = Convert.ToInt32(inputSecond);
+int square = numberFirst * numberFirst;
+if (numberSecond == square) {
+   Console.WriteLine($"Число {numberSecond} является квадратом числа {numberFirst}");
+}
+else {
+    Console.WriteLine($"Число {numberSecond} не является квадратом числа {numberFirst}");
+}
